02c41955f92dbd51707ecf8a0161d9381c9fd785
besh612 <sung.hun5016@gmail.com>
2021-04-14 17:13:10 +0900

    [modify] Copyright text
    Copyright 문구를 이름으로 수정, 년도 출력 방식을 백팁으로 바꿈

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Link from "@material-ui/core/Link";
 
-const Copyright = () => (
-  <Typography variant="body2" color="textSecondary" align="center">
-    {"Copyright © "}
-    <Link color="inherit" href="https://github.com/besh612">
-      캡스톤 잘할꼬야
-    </Link>{" "}
-    {new Date().getFullYear()}.
-  </Typography>
-);
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/besh612">
+        배성훈
+      </Link>
+      {` ${new Date().getFullYear()}.`}
+    </Typography>
+  );
+}
 
 export default Copyright;
