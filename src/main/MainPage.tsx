abfea4a213fb99a9a123b5c1e88c256d97dc860c
besh612 <sung.hun5016@gmail.com>
2021-04-14 17:56:10 +0900

    [add] test get data from api
    api get테스트용

--- src/main/MainPage.tsx
+++ src/main/MainPage.tsx
@@ -1,9 +1,26 @@
-import * as React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+import server from "../config/credentials.json";
 
 function MainPage() {
+  const [data, setData] = useState(null);
+  const handleGetData = async () => {
+    try {
+      const response = await axios.get(`${server.url}user/1`);
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div>
       <h1>여기는 메인페이지</h1>
+      <button type="button" onClick={handleGetData}>
+        불러오기
+      </button>
+      {data && <textarea value={JSON.stringify(data, null, 2)} readOnly />}
     </div>
   );
 }
