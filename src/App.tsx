17589a47b178333f4d25d578226377a13a7c8445
besh612 <sung.hun5016@gmail.com>
2021-05-14 00:10:37 +0900

    [update] import codes
    react 임포트 방식 변경, useState 임포트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -33,9 +33,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function App() {
+function App(): React.ReactElement {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = useState(true);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
